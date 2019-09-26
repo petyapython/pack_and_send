07e08185516d007ff75da3428d7b453c6bd769fc
crunch_42 <petya.python@gmail.com>
2019-09-27 00:13:41 +0300

    first commnit

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('hello World!')
+import pack
+import send
+import configparser
+import argparse
+import pathlib
+from collections import namedtuple
+
+def main():
+    My_email = namedtuple('My_email' , 'fromaddr password toaddr filepath ')
+    config = configparser.ConfigParser()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d',type=bool)
+    args = parser.parse_args()
+    current_dir = pathlib.Path.cwd()
+    config.read(str(current_dir)+'\config.ini')
+    print(str(current_dir)+'config.ini')
+    my_emal = My_email(fromaddr = config['EMAIL']['username'],
+                       password = config['EMAIL']['userpassword'],
+                       toaddr= config['EMAIL']['username'],
+                       filepath= pack.main(str(current_dir)))
+    print(my_emal)
+    send.main(my_emal)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ pack.py
@@ -0,0 +1,19 @@
+import os
+import tarfile
+import datetime
+
+def tardir(path, file_name):
+    with tarfile.open(file_name, "w:gz") as tar_handle:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                tar_handle.add(os.path.join(root, file))
+
+
+
+def main(tarf_path):
+    file_name = tarf_path+'\\'+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'.tar'
+    tardir(tarf_path, file_name)
+    return file_name
+
+if __name__ == '__main':
+    main(tarf_path)
